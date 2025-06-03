e8c1ddf81d34fb11db08c31aba63f5ff62cef3d1
rzzrr <athawoantra@gmail.com>
2025-06-03 20:26:51 +0300

    CurrentWeatherActivity

---
+++ app/src/main/java/com/rzatha/labo2testweatherinformer/presentation/activity/WeeklyWeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.rzatha.labo2testweatherinformer.presentation.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rzatha.labo2testweatherinformer.R
+
+class WeeklyWeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weekly_weather)
+    }
+}
